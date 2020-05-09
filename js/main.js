0532259baf4732bd39995f8c394f8814f41755f8
Sayo Kawanago <k.sayong@gmail.com>
2020-05-10 00:07:09 +0900

    sky-colorに変更

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 $(function () {
     //朝、昼、夕、夜の時間判定処理
     var getH = new Date().getHours();
-    if ((getH >= 5) && (getH < 10)) { $('.tgtCnt').removeClass('night').addClass('morning'); }
-    else if ((getH >= 10) && (getH < 16)) { $('.tgtCnt').removeClass('morning').addClass('noon'); }
-    else if ((getH >= 16) && (getH < 18)) { $('.tgtCnt').removeClass('noon').addClass('afternoon'); }
-    else if ((getH >= 18) || (getH < 5)) { $('.tgtCnt').removeClass('afternoon').addClass('night'); }
+    if ((getH >= 5) && (getH < 10)) { $('.sky-color').removeClass('night').addClass('morning'); }
+    else if ((getH >= 10) && (getH < 16)) { $('.sky-color').removeClass('morning').addClass('noon'); }
+    else if ((getH >= 16) && (getH < 18)) { $('.sky-color').removeClass('noon').addClass('afternoon'); }
+    else if ((getH >= 18) || (getH < 5)) { $('.sky-color').removeClass('afternoon').addClass('night'); }
 });
