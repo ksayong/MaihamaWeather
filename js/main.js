b9936071ee0161bfed5c739780c20caded4c185a
sayo <k.sayong86@gmail.com>
2020-05-18 21:27:45 +0900

    apiを取得するためのコードを記述

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ $(function () {
     else if ((getH >= 16) && (getH < 18)) { $('.sky-color').removeClass('noon').addClass('afternoon'); }
     else if ((getH >= 18) || (getH < 5)) { $('.sky-color').removeClass('afternoon').addClass('night'); }
 });
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        city: null,
+        temp: null,
+        condition: {
+            main: null
+        }
+    },
+    mounted: function () {
+        axios.get('https://api.openweathermap.org/data/2.5/weather?q=Urayasu,jp&units=metric&appid=b60a9e35aacf10a739593a8245b04046')
+            .then(function (response) {
+                this.city = response.data.name
+                this.temp = response.data.main.temp
+                this.condition = response.data.weather[0]
+            }.bind(this))
+            .catch(function (error) {
+                console.log(error)
+            })
+    },
+    filters: {
+        roundUp(value) {
+            return Math.ceil(value)
+        }
+    }
+})
