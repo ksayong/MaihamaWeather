54597ebf953bf96dd80fba972028ce2762665b03
sayo <k.sayong86@gmail.com>
2020-05-18 19:37:33 +0900

    修正前に戻す

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-jQuery(function ($) {
-    //時刻によってbackground-colorを変化させる
-    $(function () {
-        //朝、昼、夕、夜の時間判定処理
-        var getH = new Date().getHours();
-        if ((getH >= 5) && (getH < 10)) { $('.sky-color').removeClass('night').addClass('morning'); }
-        else if ((getH >= 10) && (getH < 16)) { $('.sky-color').removeClass('morning').addClass('noon'); }
-        else if ((getH >= 16) && (getH < 18)) { $('.sky-color').removeClass('noon').addClass('afternoon'); }
-        else if ((getH >= 18) || (getH < 5)) { $('.sky-color').removeClass('afternoon').addClass('night'); }
-    });
+//時刻によってbackground-colorを変化させる
+$(function () {
+    //朝、昼、夕、夜の時間判定処理
+    var getH = new Date().getHours();
+    if ((getH >= 5) && (getH < 10)) { $('.sky-color').removeClass('night').addClass('morning'); }
+    else if ((getH >= 10) && (getH < 16)) { $('.sky-color').removeClass('morning').addClass('noon'); }
+    else if ((getH >= 16) && (getH < 18)) { $('.sky-color').removeClass('noon').addClass('afternoon'); }
+    else if ((getH >= 18) || (getH < 5)) { $('.sky-color').removeClass('afternoon').addClass('night'); }
 });
